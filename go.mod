a56d4f935af65055570486f67db8c28da1332e7f
Daniel Pacak <pacak.daniel@gmail.com>
2020-07-01 22:34:30 +0200

    feat: Integrate Trivy as a vulnerability scanner
    Resolves: #6
    
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aquasecurity/starboard-security-operator
 go 1.14
 
 require (
-	github.com/aquasecurity/starboard v0.2.3
+	github.com/aquasecurity/starboard v0.2.6-0.20200703201715-f87b3541ccf7
 	github.com/caarlos0/env/v6 v6.2.2
 	github.com/google/uuid v1.1.1
 	k8s.io/api v0.19.0-alpha.3
--- pkg/starboard/operator.go
+++ pkg/starboard/operator.go
@@ -1,8 +1,15 @@
 package starboard
 
 import (
+	"errors"
 	"fmt"
+	"k8s.io/klog"
+
 	"github.com/aquasecurity/starboard-security-operator/pkg/scanner/vulnerability/aqua"
+	"github.com/aquasecurity/starboard/pkg/find/vulnerabilities"
+	"github.com/aquasecurity/starboard/pkg/find/vulnerabilities/trivy"
+	"github.com/aquasecurity/starboard/pkg/kube"
+
 	pods "github.com/aquasecurity/starboard/pkg/kube/pod"
 
 	starboard "github.com/aquasecurity/starboard/pkg/generated/clientset/versioned"
@@ -45,8 +52,13 @@ func (o *Operator) Run() error {
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactoryWithOptions(kubeClientset, o.config.Operator.DefaultResync)
 
 	pods := pods.NewPodManager(kubeClientset)
-	scanner := aqua.NewScanner(o.config, pods, aqua.NewConverter(o.config.ScannerAquaCSP))
-	starboard := action.NewStarboard(o.config, kubeClientset, starboardClientset, pods, scanner)
+
+	vulnerabilityScanner, err := o.getEnabledVulnerabilityScanner(kubeClientset, pods)
+	if err != nil {
+		return fmt.Errorf("getting vulnerability scanner: %w", err)
+	}
+
+	starboard := action.NewStarboard(o.config, kubeClientset, starboardClientset, pods, vulnerabilityScanner)
 	podController := pod.NewController(kubeInformerFactory.Core().V1().Pods(), starboard)
 	jobController := job.NewController(kubeInformerFactory.Batch().V1().Jobs(), starboard)
 
@@ -63,4 +75,22 @@ func (o *Operator) Run() error {
 
 	<-stopCh
 	return nil
+}
+
+func (o *Operator) getEnabledVulnerabilityScanner(kubeClientset kubernetes.Interface, pods *pods.Manager) (vulnerabilities.ScannerAsync, error) {
+	if o.config.ScannerTrivy.Enabled && o.config.ScannerAquaCSP.Enabled {
+		return nil, fmt.Errorf("invalid configuration: multiple vulnerability scanners enabled")
+	}
+	if !o.config.ScannerTrivy.Enabled && !o.config.ScannerAquaCSP.Enabled {
+		return nil, fmt.Errorf("invalid configuration: none vulnerability scanner enabled")
+	}
+	if o.config.ScannerTrivy.Enabled {
+		klog.Infof("Using Trivy %s as vulnerability scanner", o.config.ScannerTrivy.Version)
+		return trivy.NewScanner(kube.ScannerOpts{}, kubeClientset), nil
+	}
+	if o.config.ScannerAquaCSP.Enabled {
+		klog.Infof("Using Aqua CSP %s as vulnerability scanner", o.config.ScannerAquaCSP.Version)
+		return aqua.NewScanner(o.config, pods, aqua.NewConverter(o.config.ScannerAquaCSP)), nil
+	}
+	return nil, errors.New("invalid configuration: unhandled vulnerability scanners config")
 }
