2ed157a458decd5706ad1a6ca9b9dcfcf5e21176
Daniel Pacak <pacak.daniel@gmail.com>
2020-07-17 16:21:59 +0200

    feat: Set ownerReferences for vulnerability reports created by the operator
    Resolves: #19
    
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- pkg/reports/store.go
+++ pkg/reports/store.go
@@ -4,8 +4,18 @@ import (
 	"context"
 	"fmt"
 
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/api/batch/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+
 	starboardv1alpha1 "github.com/aquasecurity/starboard/pkg/apis/aquasecurity/v1alpha1"
 	"github.com/aquasecurity/starboard/pkg/find/vulnerabilities"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
 	"github.com/aquasecurity/starboard/pkg/kube"
 	"github.com/google/uuid"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -14,23 +24,30 @@ import (
 )
 
 type StoreInterface interface {
-	Write(ctx context.Context, workload kube.Object, vulnerabilities vulnerabilities.WorkloadVulnerabilities) error
+	Write(ctx context.Context, workload kube.Object, reports vulnerabilities.WorkloadVulnerabilities) error
 	Read(ctx context.Context, workload kube.Object) (vulnerabilities.WorkloadVulnerabilities, error)
 }
 
 type Store struct {
 	client client.Client
+	scheme *runtime.Scheme
 }
 
-func NewStore(client client.Client) *Store {
+func NewStore(client client.Client, scheme *runtime.Scheme) *Store {
 	return &Store{
 		client: client,
+		scheme: scheme,
 	}
 }
 
 func (s *Store) Write(ctx context.Context, workload kube.Object, reports vulnerabilities.WorkloadVulnerabilities) error {
+	owner, err := s.getRuntimeObjectFor(ctx, workload)
+	if err != nil {
+		return err
+	}
+
 	for container, report := range reports {
-		err := s.client.Create(ctx, &starboardv1alpha1.Vulnerability{
+		vulnerabilityReport := &starboardv1alpha1.Vulnerability{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      fmt.Sprintf(uuid.New().String()),
 				Namespace: workload.Namespace,
@@ -42,7 +59,13 @@ func (s *Store) Write(ctx context.Context, workload kube.Object, reports vulnera
 				},
 			},
 			Report: report,
-		})
+		}
+		err = controllerutil.SetOwnerReference(owner, vulnerabilityReport, s.scheme)
+		if err != nil {
+			return err
+		}
+
+		err := s.client.Create(ctx, vulnerabilityReport)
 		if err != nil {
 			return err
 		}
@@ -69,4 +92,33 @@ func (s *Store) Read(ctx context.Context, workload kube.Object) (vulnerabilities
 		}
 	}
 	return reports, nil
+}
+
+func (s *Store) getRuntimeObjectFor(ctx context.Context, workload kube.Object) (metav1.Object, error) {
+	var obj runtime.Object
+	switch workload.Kind {
+	case kube.KindPod:
+		obj = &corev1.Pod{}
+	case kube.KindReplicaSet:
+		obj = &appsv1.ReplicaSet{}
+	case kube.KindReplicationController:
+		obj = &corev1.ReplicationController{}
+	case kube.KindDeployment:
+		obj = &appsv1.Deployment{}
+	case kube.KindStatefulSet:
+		obj = &appsv1.StatefulSet{}
+	case kube.KindDaemonSet:
+		obj = &appsv1.DaemonSet{}
+	case kube.KindCronJob:
+		obj = &v1beta1.CronJob{}
+	case kube.KindJob:
+		obj = &batchv1.Job{}
+	default:
+		return nil, fmt.Errorf("unknown workload kind: %s", workload.Kind)
+	}
+	err := s.client.Get(ctx, types.NamespacedName{Name: workload.Name, Namespace: workload.Namespace}, obj)
+	if err != nil {
+		return nil, err
+	}
+	return obj.(metav1.Object), nil
 }
